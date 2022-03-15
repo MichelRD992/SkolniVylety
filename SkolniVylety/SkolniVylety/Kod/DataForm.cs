9f522cf5453a8adcfe177b46efcfea1030d44971
michelrd992 <michelrd992@gmail.com>
2022-03-15 12:12:28 +0100

    Oprava

--- SkolniVylety/SkolniVylety/Kod/DataForm.cs
+++ SkolniVylety/SkolniVylety/Kod/DataForm.cs
@@ -9,44 +9,60 @@ namespace SkolniVylety
 {
     public class DataForm : StackLayout
     {
-        public async void SetDataAsync(IData record)
+        public async void SetData(IData record)
         {
             var typ = record.GetType();
-            bool reference = false;
             foreach (var prop in typ.GetProperties())
             {
-                var val = prop.GetValue(record);
                 if (prop.IsDefined(typeof(SkrytVeFormulariAttribute)))
                     continue;
                 View editor = null;
-                if (prop.PropertyType == typeof(string) || prop.PropertyType == typeof(int))
-                    editor = new Label() { Text = (string)val };
+                if (prop.IsDefined(typeof(EnumAttribute)))
+                {
+                    editor = new Picker()
+                    {
+                        ItemsSource = "1.,2.,3.,4.,5.,6.,7.,8.,9.".Split(',')
+                    };
+                    editor.SetBinding(Picker.SelectedItemProperty, prop.Name);
+                }
                 else if (prop.PropertyType == typeof(int?) && prop.IsDefined(typeof(ReferenceAttribute)))
                 {
-                    string nazev;
-                    if (!reference)
+                    editor = new LookupPicker()
                     {
-                        List<Trida> trida = await DBUtils.DB.Table<Trida>().Where(x => x.ID == (int)val).ToListAsync();
-                        nazev = trida[0].Nazev;
-                        reference = true;
-                    }
-                    else
-                    {
-                        List<Vylet> vylet = await DBUtils.DB.Table<Vylet>().Where(x => x.ID == (int)val).ToListAsync();
-                        nazev = vylet[0].Nazev;
-                    }
-                    editor = new Label() { Text = nazev };
+                        ItemsSource = await DBUtils.DataTabulky(
+                            prop.GetCustomAttribute<ReferenceAttribute>().Table) as IList
+                    };
+                    editor.SetBinding(LookupPicker.IDProperty, prop.Name);
+                }
+                else if (prop.PropertyType == typeof(string))
+                {
+                    editor = new Entry();
+                    editor.SetBinding(Entry.TextProperty, prop.Name);
+                }
+                else if (prop.PropertyType == typeof(int))
+                {
+                    editor = new Entry() { Keyboard = Keyboard.Numeric };
+                    editor.SetBinding(Entry.TextProperty, prop.Name);
                 }
                 else if (prop.PropertyType == typeof(DateTime))
-                    editor = new Label() { Text = DateTime.Parse((string)val).ToString("d.m.yyyy") };
+                {
+                    editor = new DatePicker() { HorizontalOptions = LayoutOptions.Start };
+                    editor.SetBinding(DatePicker.DateProperty, prop.Name);
+                }
+                else if (prop.PropertyType == typeof(bool))
+                {
+                    editor = new Switch();
+                    editor.SetBinding(Switch.IsToggledProperty, prop.Name);
+                }
                 if (editor != null)
                 {
-                    Children.Add(new Label() { Text = DBUtils.Popisek(prop) + ":" });
+                    Children.Add(new Label() { Text = DBUtils.Popisek(prop) });
                     editor.Margin = new Thickness(0, 2, 0, 10);
                     Children.Add(editor);
                 }
             }
+            BindingContext = record;
         }
-        //public IData GetData() => BindingContext as IData;
+        public IData GetData() => BindingContext as IData;
     }
 }
